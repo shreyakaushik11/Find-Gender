6529af25683f69ef516c6d05d8a9b7e0346141bf
Shreya Kaushik <shreyakaushik11@gmail.com>
2018-07-02 10:02:28 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  url="https://api.genderize.io/?name=";
+  name;
+  gender;
+  constructor(private http: HttpClient){
+
+  }
+  getData(){
+    this.http.get(this.url+this.name).subscribe((data)=>{
+      console.log(data);
+      this.gender=data['gender'];
+      
+    })
+  }
 }
